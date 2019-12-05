d314627f0a31f1350b9791c0d104fa02adf97a5f
Alex Tsiras <tsirasalexis@yahoo.gr>
2019-12-05 09:00:09 +0200

    Fix params

--- lib/simple_calendar/calendar.rb
+++ lib/simple_calendar/calendar.rb
@@ -8,7 +8,7 @@ module SimpleCalendar
 
     def initialize(view_context, opts={})
       Rails.logger.info "DEBUG:: ------------------------------------"
-      Rails.logger.info "DEBUG:: view_context: #{view_context}"
+      Rails.logger.info "DEBUG:: view_context: #{view_context.inspect}"
 
       @view_context = view_context
       @options = opts
@@ -17,8 +17,8 @@ module SimpleCalendar
       @params = @view_context.respond_to?(:params) ? @view_context.params : Hash.new
       @params = @params.to_unsafe_h if @params.respond_to?(:to_unsafe_h)
       @params = @params.with_indifferent_access.except(*PARAM_KEY_BLACKLIST)
-      Rails.logger.info "DEBUG:: opts: #{opts}"
-      Rails.logger.info "DEBUG:: params: #{params}"
+      Rails.logger.info "DEBUG:: opts: #{@options}"
+      Rails.logger.info "DEBUG:: params: #{@params}"
       Rails.logger.info "DEBUG:: ------------------------------------"
 
       # Add in any additonal params the user passed in
